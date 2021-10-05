787e8a0961986e1d585527e7b5c9b8045c3adb78
Danny <doihebunandu@sidmach.com>
2021-10-05 12:57:51 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,40 @@
+require('./bootstrap');
+
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default },
+    { path: '/users', component: require('./components/Users.vue').default },
+]
+
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router' // Import Vue Router for Use Vue Router
+Vue.use(VueRouter)
+
+// Path routes javascript same route in Laravel PHP
+
+
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+
+const router = new VueRouter({
+    mode: 'history', // use for history for bu
+    routes
+})
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+Vue.component('example-component', require('./components/ExampleComponent.vue')); // Componnet for example
+
+const app = new Vue({
+    el: '#app',
+    router
+});
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Table</h3>
+
+                <div class="card-tools">
+                    <button class="btn btn-success" data-toggle="modal" data-target="#addNew"><i class="fas fa-user-plus"> Add New</i></button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                        <a href="#" class="btn">
+                            <i class="fas fa-edit yellow"></i> Edit
+                        </a>
+                        <a href="#" class="btn">
+                            <i class="fas fa-trash red"></i> Delete
+                        </a>
+                      </td>
+                    </tr>
+
+
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+
+
+
+
+    <!-- Modal Area -->
+<div class="modal" id="addNew">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Add New User</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Create</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
